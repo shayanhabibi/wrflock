b0a80a00c71410bbed75f05e23db172f7fbb8868
shayanhabibi <shayanftw@gmail.com>
2021-12-21 00:17:28 +0800

    ci darwin

--- wrflock/futex/futex_darwin.nim
+++ wrflock/futex/futex_darwin.nim
@@ -31,7 +31,10 @@ proc ulock_wait(operation: uint32; address: pointer; value: uint64;
 proc ulock_wake(operation: uint32; address: pointer; wake_value: uint64): cint {.importc:"__ulock_wake", cdecl.}
 
 proc wait*[T](monitor: ptr T; compare: T; time: static int = 0): bool {.inline, discardable.} =
-  result = not(ulock_wait(UL_UNFAIR_LOCK64_SHARED, monitor, cast[uint64](compare), (time * 1000).uint32) != 0.cint)
+  when time == 0:
+    result = not(ulock_wait(UL_UNFAIR_LOCK64_SHARED, monitor, cast[uint64](compare), (high(uint32)).uint32) != 0.cint)
+  else:
+    result = not(ulock_wait(UL_UNFAIR_LOCK64_SHARED, monitor, cast[uint64](compare), (time * 1000).uint32) != 0.cint)
 
     
 
